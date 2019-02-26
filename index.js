4bbbedc7b3faca4be81145b658e24e8b21bedfaa
Cheng Vue <csalvue@gmail.com>
2019-02-26 23:12:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ class Neighborhood {
 }
 
 class Customer {
-  constructor(name, neighborhood) {
+  constructor(name, neighborhoodId) {
     this.id = ++customerId;
     this.name = name;
-    this.neighborhoodId = neighborhood.id;
+    this.neighborhoodId = neighborhoodId;
   }
 }
 
